fd44cf009dbc24ed2c60957f726672a06af21247
Will Dailey <willdailey14@macbook-pro-5.local>
2017-10-31 17:11:08 -0400

    update for week 10

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Will Dailey on 10/24/17.
+//  Copyright © 2017 Will Dailey. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "a073db8973150fe3936a28410a0b0d40/"
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,84 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Will Dailey on 10/23/17.
+//  Copyright © 2017 Will Dailey. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+
+class WeatherLocation {
+    
+    struct DailyForecast {
+        var dailyMaxTemp: Double
+        var dailyMinTemp: Double
+        var dailyDate: Double
+        var dailySummary: String
+        var dailyIcon: String
+    }
+    
+    var name = ""
+    var coordinates = ""
+    var currentTemp = "--"
+    var currentSummary = ""
+    var currentIcon = ""
+    var currentTime = 0.0
+    var timeZone = ""
+    var dailyForecastArray = [DailyForecast]()
+    
+    func getWeather(completed: @escaping () -> ()) {
+        let weatherURL = urlBase + urlAPIKey + coordinates
+        
+        Alamofire.request(weatherURL).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                if let temperature = json["currently"]["temperature"].double {
+                    let roundedTemp = String(format: "%3.f", temperature)
+                    self.currentTemp = roundedTemp + "°"
+                } else {
+                    print("could not return temp")
+                }
+                if let summary = json["daily"]["summary"].string {
+                    self.currentSummary = summary
+                } else {
+                    print("could not print summary")
+                }
+                if let icon = json["currently"]["icon"].string {
+                    self.currentIcon = icon
+                } else {
+                    print("could not print icon")
+                }
+                if let timeZone = json["timezone"].string {
+                    self.timeZone = timeZone
+                } else {
+                    print("could not get timezone")
+                }
+                if let time = json["currently"]["time"].double {
+                    self.currentTime = time
+                } else {
+                    print("could not print icon")
+                }
+                let dailyDataArray = json["daily"]["data"]
+                self.dailyForecastArray = []
+                for day in 1...dailyDataArray.count-1 {
+                    let maxTemp = json["daily"]["data"][day]["temperatureHigh"].doubleValue
+                    let minTemp = json["daily"]["data"][day]["temperatureLow"].doubleValue
+                    let dateValue = json["daily"]["data"][day]["time"].doubleValue
+                    let icon = json["daily"]["data"][day]["icon"].stringValue
+                    let dailySummary = json["daily"]["data"][day]["summary"].stringValue
+                    let newDailyForecast = DailyForecast(dailyMaxTemp: maxTemp, dailyMinTemp: minTemp, dailyDate: dateValue, dailySummary: dailySummary, dailyIcon: icon)
+                    self.dailyForecastArray.append(newDailyForecast)
+                }
+            case .failure(let error):
+                print(error)
+            }
+            completed()
+        }
+    }
+    
+}
